699e967a7d8a0228dd1b099f39af5bb119880a9c
abhijith <obhijithjana@gmail.com>
2023-10-03 11:14:06 +0530

    env setup

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+ const config={
+APPWRITE_URL:String(import.meta.env.VITE_APPWRITE_URL),
+PROJECT_ID:String(import.meta.env.VITE_PROJECT_ID),
+DATABASE_ID:String(import.meta.env.VITE_DATABASE_ID),
+COLLECTION_ID:String(import.meta.env.VITE_COLLECTION_ID),
+BUCKET_ID:String(import.meta.env.VITE_BUCKET_ID)
+}
+export default config;
